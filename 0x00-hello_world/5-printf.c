2791df7544f7b9a0d94b875045f0d01beccffadf
Luguspro <juliuslugemwa42@gmail.com>
2023-07-14 16:54:20 -0700

    C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stio.h>
+/**
+ * main - program that prints exactly with proper grammar, but the outcome is a piece of art using printf function
+ * return: o(success)
+ */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
