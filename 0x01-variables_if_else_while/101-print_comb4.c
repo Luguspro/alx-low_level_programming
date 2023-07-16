032bba3c406a369e0edcc7b79cd9bbfa3a3eb143
Luguspro <juliuslugemwa42@gmail.com>
2023-07-16 08:36:29 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,7 @@ int main(void)
 		{
 			for (c = 0; c < 10; c++)
 			{
-				if ((a != b) || (b != c) || (a != c) || ((a < (b & C)) & (b < c)))
+				if ((a != b) || (b != c) || (a != c) || ((a < (b && c)) && (b < c)))
 				{
 					putchar(a);
 					putchar(b);
