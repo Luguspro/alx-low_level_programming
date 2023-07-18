df4546cc4d9a51c530824de23e4b67ea0c122802
Luguspro <juliuslugemwa42@gmail.com>
2023-07-18 14:50:53 -0700

    C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - printout alphabet
+ * Return: 0(success)
+ */
+void print_alphabet(void);
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+		_putchar('\n');
+	}
+	return (0);
+}
