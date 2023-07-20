d7b2988d3943fb4127dbc89b7adc13e8eac575f8
Luguspro <juliuslugemwa42@gmail.com>
2023-07-20 06:07:39 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: integer a
+ * @b: integer b
+ * Return: 0 always
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function that print 0 to 9 excluding 4 and 2
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		if ((a != 2) || (a != 4))
+		{
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
