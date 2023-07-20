75a2c8246e42f92dbb3d674619138ac4b19e54a4
Luguspro <juliuslugemwa42@gmail.com>
2023-07-20 15:15:52 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,11 +16,11 @@ void print_diagonal(int n)
 	}
 	for (a = 0; a < n; a++)
 	{
-		for (b = 0; b <	n; b++)
+		for (b = 0; b <	a; b++)
 		{
 			_putchar(' ');
 		}
-		_putchar('\\');
+		_putchar('\');
 		_putchar('\n');
 	}
 }
