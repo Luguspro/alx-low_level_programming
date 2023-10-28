7b33548ade38457dfa7a592a0076d23a7b712470
Luguspro <juliuslugemwa42@gmail.com>
2023-10-28 14:25:53 -0700

    To returns the value of a bit at a given index

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,10 +2,10 @@
 
 /**
 * get_bit - function to return value of a bit to a given index
-* @n: number to be searched for
-* @index: index of the bit
+* @n: number to search for
+* @index: Bit index
 *
-* Return: value of the bit at index
+* Return: The bit at index
 */
 
 int get_bit(unsigned long int n, unsigned int index)
@@ -13,7 +13,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bit_val;
 
 if (index > 63)
-	return (-1);
+return (-1);
 bit_val = (n >> index) & 1;
 return (bit_val);
 }
