ca4b9b8a33ef8754e3035bd8f450feb42fd429a8
LUGEMWA juliuslugemwa42@gmail.com <137568277+Luguspro@users.noreply.github.com>
2023-10-28 18:40:39 +0300

    0x14-Bit_manipulation

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* flip_bits - function to count the number of bits to change
+* to get from one number to another
+* @n: first number
+* @m: second number
+*
+* Return: number of bits to flip
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a = 63, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+	
+while (i >= 0)
+{
+	current = exclusive >> i;
+	if (current & 1)
+		count++;
+	i--;
+}
+return (count);
+}
+
